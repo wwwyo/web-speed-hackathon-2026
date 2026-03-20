628e54766f0e94ca3d1cfba95789e8532e91bb64
yuito.watanabe <w.ytmrm11811@gmail.com>
2026-03-20 22:44:23 +0900

    fix: Link コンポーネントを react-router Link に置換しSPAナビゲーションを有効化

--- application/client/src/components/foundation/Link.tsx
+++ application/client/src/components/foundation/Link.tsx
@@ -1,13 +1,12 @@
 import { AnchorHTMLAttributes, forwardRef } from "react";
-import { To, useHref } from "react-router";
+import { Link as RouterLink, To } from "react-router";
 
 type Props = Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "href"> & {
   to: To;
 };
 
 export const Link = forwardRef<HTMLAnchorElement, Props>(({ to, ...props }, ref) => {
-  const href = useHref(to);
-  return <a ref={ref} href={href} {...props} />;
+  return <RouterLink ref={ref} to={to} {...props} />;
 });
 
 Link.displayName = "Link";

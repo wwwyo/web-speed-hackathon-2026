07d5def7ef9d0fb61d7bb0be2ae108805e1c4ba5
yuito.watanabe <w.ytmrm11811@gmail.com>
2026-03-21 16:00:37 +0900

    fix: NewPostModalContainer の toggle resetKey を close 時のみに限定
    モーダル open 時にも resetKey が更新されフォームが再マウントされる
    race condition を修正。scoring tool の投稿フロー安定化。

--- application/client/src/containers/NewPostModalContainer.tsx
+++ application/client/src/containers/NewPostModalContainer.tsx
@@ -40,8 +40,10 @@ export const NewPostModalContainer = ({ id }: Props) => {
     }
 
     const handleToggle = () => {
-      // モーダル開閉時にkeyを更新することでフォームの状態をリセットする
-      setResetKey((key) => key + 1);
+      // モーダルが閉じた時だけkeyを更新してフォームの状態をリセットする
+      if (!element.open) {
+        setResetKey((key) => key + 1);
+      }
     };
     element.addEventListener("toggle", handleToggle);
     return () => {
